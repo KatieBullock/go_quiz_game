11237e3faea2d69c9435c6d581115b8ba493d3d3
KatieBullock <katie.bullock@rushdownstudio.com>
2026-05-07 13:39:25 -0400

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/goquizgame
+
+go 1.25.1
---
+++ quiz_game.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("problems.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close() // NOTE(Katie): What is happening here? The file is being closed, by why here and why defer?
+
+	reader := csv.NewReader(file)
+
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(record)
+	}
+}
