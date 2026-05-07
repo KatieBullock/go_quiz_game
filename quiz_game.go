543ea9f29a3646cef01e0405ac01be976a807b90
KatieBullock <katie.bullock@rushdownstudio.com>
2026-05-07 14:36:35 -0400

    Add csv flag, input, and answers correct feedback

--- quiz_game.go
+++ quiz_game.go
@@ -2,20 +2,29 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"io"
 	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
-	file, err := os.Open("problems.csv")
+	csvPtr := flag.String("csv", "problems.csv", "a csv in the format of 'question,answer'")
+
+	flag.Parse()
+
+	file, err := os.Open(*csvPtr)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close() // NOTE(Katie): What is happening here? The file is being closed, by why here and why defer?
 
 	reader := csv.NewReader(file)
+
+	correct := 0
+	count := 0
 
 	for {
 		record, err := reader.Read()
@@ -26,6 +35,17 @@ func main() {
 			log.Fatal(err)
 		}
 
-		fmt.Println(record)
+		count++
+
+		fmt.Printf("Problem #" + strconv.Itoa(count) + ": " + record[0] + " = ")
+
+		var answer string
+		fmt.Scanln(&answer)
+
+		if answer == record[1] {
+			correct += 1
+		}
 	}
+
+	fmt.Println("You scored " + strconv.Itoa(correct) + " out of " + strconv.Itoa(count))
 }
