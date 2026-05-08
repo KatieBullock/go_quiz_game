252dc304df5fd30b91af615238adb65ad55795d6
KatieBullock <katie.bullock@rushdownstudio.com>
2026-05-08 13:22:06 -0400

    Add smaller quiz function to remove loop label

--- quiz_game.go
+++ quiz_game.go
@@ -5,38 +5,18 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"strings"
 	"time"
 )
 
-func main() {
-	csvPtr := flag.String("csv", "problems.csv", "a csv in the format of 'question,answer'")
-	limitPtr := flag.Int64("limit", 30, "time limit in seconds to answer questions")
-
-	flag.Parse()
-
-	file, err := os.Open(*csvPtr)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	reader := csv.NewReader(file)
-	records, err := reader.ReadAll()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Press [Enter] to Begin!")
-	fmt.Scanln()
-
-	timeout := time.After(time.Duration(*limitPtr) * time.Second)
+func quiz(timeLimit int, records [][]string) int {
+	timeout := time.After(time.Duration(timeLimit) * time.Second)
 	correct := 0
 
-quizLoop:
 	for i, record := range records {
-		fmt.Printf("Problem # %d: %s = ", i+1, record[0])
+		fmt.Printf("Problem #%d: %s = ", i+1, record[0])
 
 		inputChan := make(chan string)
 
@@ -52,9 +32,42 @@ quizLoop:
 				correct++
 			}
 		case <-timeout:
-			break quizLoop
+			return correct
 		}
 	}
+
+	return correct
+}
+
+func main() {
+	csvPtr := flag.String("csv", "problems.csv", "a csv in the format of 'question,answer'")
+	timeLimitPtr := flag.Int("limit", 30, "a time limit in seconds")
+	shufflePtr := flag.Bool("shuffle", false, "a 'true' or 'false' to shuffle questions")
+
+	flag.Parse()
+
+	file, err := os.Open(*csvPtr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if *shufflePtr {
+		rand.Shuffle(len(records), func(i, j int) {
+			records[i], records[j] = records[j], records[i]
+		})
+	}
+
+	fmt.Println("Press [Enter] to Begin!")
+	fmt.Scanln()
+
+	correct := quiz(*timeLimitPtr, records)
 
 	fmt.Printf("\nYou scored %d out of %d\n", correct, len(records))
 }
